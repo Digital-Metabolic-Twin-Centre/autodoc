3834b10987b5147bd6dc691392efdbea3853492b
chidi esobe <chidiesobe@gmail.com>
2026-07-17 12:20:27 +0100

    chore: design update fixed position for logo

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_css_files = ["custom-wide.css"]
 html_logo = "_static/img/logo.png"
-html_favicon = "_static/img/favicon.ico"
+html_favicon = "_static/img/logo.png"
 html_theme_options = {
     "collapse_navigation": False,
     "navigation_depth": 4,
